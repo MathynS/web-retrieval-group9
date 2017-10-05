aa30b1a5963f5003122866e19869bc232b0e8564
Mathyn Scheerder <mathyn@scheerder.biz>
2017-10-05 14:30:51 +0200

    Revised commit for server

---
+++ server/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function csv_to_array($filename='', $delimiter=',')
+    {
+        if(!file_exists($filename) || !is_readable($filename))
+            return FALSE;
+
+        $header = NULL;
+        $data = array();
+        if (($handle = fopen($filename, 'r')) !== FALSE)
+        {
+            while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+            {
+                if(!$header)
+                    $header = $row;
+                else
+                    $data[] = array_combine($header, $row);
+            }
+            fclose($handle);
+        }
+        return $data;
+ 	}
+
+
+    public function run()
+    {
+    	$csv_file = public_path() . '/csv/papers.csv';
+    	$papers = $this->csv_to_array($csv_file);
+        DB::table('documents')->insert($papers);
+
+    }
+}
