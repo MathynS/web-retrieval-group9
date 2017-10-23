f4294f17c77cf3a59e0b282398370abb80e0f681
ianzzz94 <ian_zhang94@hotmail.com>
2017-10-23 20:47:10 +0200

    Add Rating_crawler for h-index and i10-index

--- citations_crawler/extract.py
+++ citations_crawler/extract.py
@@ -18,10 +18,12 @@ LIMIT = 6603
 #Cookie file for less Captcha requests
 COOKIE = "cookies.txt"
 
+
 def citations_to_csv(n_citations):
     with open('citations.csv', 'a') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_MINIMAL)
         wr.writerow(n_citations)
+
 
 def scrape_citation_count(p):
     scholar.ScholarConf.COOKIE_JAR_FILE = COOKIE
@@ -37,6 +39,7 @@ def scrape_citation_count(p):
         print("Google Scholar captcha :(")
         return -1
 
+
 def main():
     papers = Papers.select().limit(LIMIT)
     n_citations = []
@@ -50,6 +53,7 @@ def main():
         time.sleep(2)
     citations_to_csv(n_citations)
     print(n_citations)
+
 
 if __name__ == '__main__':
     main()
